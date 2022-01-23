1f5945456823d3aaffa5f03b7551d013439e0c6a
Jayanta Adhikary <73451350+jayantaadhikary@users.noreply.github.com>
2022-01-24 00:36:29 +0530

    project file

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func main() {
+	links := []string{
+		"http://google.com",
+		"http://golang.org",
+		"http://facebook.com",
+		"http://stackoverflow.com",
+		"http://amazon.com",
+	}
+
+	c := make(chan string)
+
+	for _, link := range links {
+		go checkLink(link, c)
+	}
+
+	for l := range c {
+		//watches for the channnel c for when a value comes out of it. Assigns it to l. And then executes the body of the for loop
+
+		go func(link string) { //function literal
+			time.Sleep(3 * time.Second) //pauses a go routine for a specific amount of duration.
+			checkLink(link, c)
+		}(l)
+	}
+}
+
+func checkLink(link string, c chan string) {
+	_, err := http.Get(link)
+	if err != nil {
+		fmt.Println(link, "might be down!")
+		c <- link
+		return
+	}
+
+	fmt.Println(link, "is up!")
+	c <- link
+}
